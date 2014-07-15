efc6f9eda88dd6eb388ce195502fa43c3147d3e4
KostasDasios <gostaz13@gmail.com>
2014-07-15 16:38:30 +0300

    Add rest apifor courses

--- modules/rest/courses.php
+++ modules/rest/courses.php
@@ -1,6 +1,12 @@
 <?php
 function GetCourses() {
-    echo 'GET';
+	$database = Database::get();
+	$course=array();
+	$database->queryFunc(
+	                    'SELECT title FROM course',
+						function($row) use (&$courses) {$courses[]=$row;}
+	                      );
+	echo json_encode($courses);
 }
 function PostCourses() {
     echo 'POST';
